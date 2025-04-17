3960b7eec498c18bca615972aba9b71d74bbddfc
Erik <EH2004@gmail.com>
2025-04-17 17:20:11 -0500

    Changed QuintAtomType to non-local name

---
+++ postbuild.bat
@@ -0,0 +1,3 @@
+rem Change "Release" into "Debug" or vice
+copy %~dp0bin\Release\net452\Quintessential.dll "C:\Program Files (x86)\Steam\steamapps\common\Opus Magnum\Quintessential.dll"
+rem You still have to run "OpusMutatum.exe -merge"
