6b9311a512cf29b1f27f55cc0fd8f8a6f5a2a216
Erik <EH2004@gmail.com>
2025-04-17 19:53:50 -0500

    Removed postbuild from project

--- postbuild.bat
+++
@@ -1,3 +0,0 @@
-rem Change "Release" into "Debug" or vice
-copy %~dp0bin\Release\net452\Quintessential.dll "C:\Program Files (x86)\Steam\steamapps\common\Opus Magnum\Quintessential.dll"
-rem You still have to run "OpusMutatum.exe -merge"
